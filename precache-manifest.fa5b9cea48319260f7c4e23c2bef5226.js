5c7a4c89bbab911f1d4a78e53e88e94f83e45594
Code Sandbox <hello@codesandbox.io>
2022-03-08 10:02:52 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fa5b9cea48319260f7c4e23c2bef5226.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d071c3d6ab0b7c0ad7df366e05cf6f",
+    "url": "https://antoinelaly.github.io/csb-cyw66c/index.html"
+  },
+  {
+    "revision": "253d3635c151ef3dd75f",
+    "url": "https://antoinelaly.github.io/csb-cyw66c/static/css/main.591722c4.chunk.css"
+  },
+  {
+    "revision": "e2a95fbecc1f29f1a581",
+    "url": "https://antoinelaly.github.io/csb-cyw66c/static/js/2.d4c17f44.chunk.js"
+  },
+  {
+    "revision": "8d98233dfb2f1a8b874a10cb1448b7bd",
+    "url": "https://antoinelaly.github.io/csb-cyw66c/static/js/2.d4c17f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253d3635c151ef3dd75f",
+    "url": "https://antoinelaly.github.io/csb-cyw66c/static/js/main.5dc15325.chunk.js"
+  },
+  {
+    "revision": "e61a1f5fc0033baa66a3",
+    "url": "https://antoinelaly.github.io/csb-cyw66c/static/js/runtime-main.8e693d10.js"
+  },
+  {
+    "revision": "a552303ddb5867c86c7ac5498c8191e7",
+    "url": "https://antoinelaly.github.io/csb-cyw66c/static/media/times-solid.a552303d.svg"
+  }
+]);
